20888015e8aa69734b55aee34e9d35903efca179
genevievehg <genevievegarwood@gmail.com>
2026-08-07 16:56:54 +0100

    feat: add retrieval step to chatbot and improve prompt handling

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -1,6 +1,8 @@
 import logging
 from chatbot import Chatbot
 from classifier import EmotionClassifier
+from sentence_transformers import SentenceTransformer
+from retriever import Retriever
 
 logging.basicConfig(filename="logs/chatbot_log.log",
                     format='%(asctime)s %(levelname)s: %(message)s')
@@ -9,24 +11,31 @@ logger.setLevel(logging.INFO)
 
 
 def chatbot_interface():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    retriever = Retriever('data/facts.txt')
+    emotion_classifier = EmotionClassifier('data/models/trained_classifier.pkl')
+    chatbot = Chatbot()
+
     print('''Chatbot Assistant: Hey! I'm here to help! \nWhat text would you like me to classify?
 You can type 'exit' to leave the chat. \nUser:''')
     prompt = input()
     while prompt != 'exit':
-        emotion_classifier = EmotionClassifier('data/models/trained_classifier.pkl')
         emotion_classification = emotion_classifier.classify_text(prompt)
+
+        embeddings = model.encode(prompt)
+
+        retrieved_chunks = retriever.retrieve_top_3_chunks(embeddings)
+
         logger.info(f'''User prompt: {prompt}''')
         logger.info(f'''Result of emotion classification: {emotion_classification}''')
-        appended_prompt = f'''\n<|user|>\n {prompt}.
-        Emotion classification: {emotion_classification} \n<|end|>\n<|assistant|>\n'''
-        logger.info(f'''Second prompt: {appended_prompt}''')
-        chatbot = Chatbot('''<|system|>\nYou are a friendly assistant.
-        You will be given a user's message and an emotion classification result.
-        Answer the user's question taking the classification result into account.<|end|>\n''')
-        reply = chatbot.generate_reply(appended_prompt)
+        logger.info(f'''Retrieved chunks: {retrieved_chunks}''')
+        
+        reply = chatbot.generate_reply(prompt, emotion_classification, retrieved_chunks)
+
         logger.info(f'''Chatbot reply: {reply}''')
+
         print(f"Bot:\n{reply}\n")
-        print("User:")
+        print(f'''Do you have any other text to classify?" \nRemember you can type 'exit' to leave the chat. \nUser:''')
         prompt = input()
 
 if __name__ == "__main__":
